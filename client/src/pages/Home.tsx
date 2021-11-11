0233499268cd8622e5543edb84c763e74f862132
mizan <dev.mizan4392@gmail.com>
2021-11-11 23:04:02 +0600

    added slider

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from "react";
 import Announcement from "../components/Announcement/Announcement.component";
 import NabBar from "../components/NavBar/NabBar.component";
-
+import Slider from "../components/Slider/Slider.component";
 interface Props {}
 
 export default function Home({}: Props): ReactElement {
@@ -9,6 +9,7 @@ export default function Home({}: Props): ReactElement {
     <div>
       <Announcement />
       <NabBar />
+      <Slider />
     </div>
   );
 }
