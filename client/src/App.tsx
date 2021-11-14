fed3d67cd644ffd19f5274d5e3d8f14dca72dcbb
mizan <dev.mizan4392@gmail.com>
2021-11-14 20:58:07 +0600

    products and product detailes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails.page";
+import ProductLists from "./pages/ProductLists.page";
 
 function App() {
-  return <Home />;
+  return <ProductDetails />;
 }
 
 export default App;
---
+++ client/src/pages/ProductLists.page.tsx
@@ -0,0 +1,81 @@
+import React, { ReactElement } from "react";
+import styled from "styled-components";
+import Announcement from "../components/Announcement/Announcement.component";
+import Footer from "../components/Footer/Footer.component";
+import NabBar from "../components/NavBar/NabBar.component";
+import Newsletter from "../components/Newsletter/Newsletter.component";
+import Products from "../components/Product/Product.component";
+interface Props {}
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``;
+
+export default function ProductLists({}: Props): ReactElement {
+  return (
+    <Container>
+      <NabBar />
+      <Announcement />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+}
